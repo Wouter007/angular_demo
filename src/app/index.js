1e5a417906742fa11e6bf341f8fb39d03ab99f29
Wouter Merckx <woutermerckx@gmail.com>
2015-03-10 17:20:59 +0100

    added custom directive

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,27 @@
 'use strict';
 
+function composeEmail () {
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: true,
+    controllerAs: 'compose',
+    controller: function () {
+
+    },
+    link: function ($scope, $element, $attrs) {
+
+    },
+    template: [
+      '<div class="compose-email">',
+        '<input type="text" placeholder="To.." ng-model="compose.to">',
+        '<input type="text" placeholder="Subject.." ng-model="compose.subject">',
+        '<textarea placeholder="Message.." ng-model="compose.message"></textarea>',
+      '</div>'
+    ].join('')
+  };
+}
+
 angular.module('angularDemo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
   
   .config(function ($stateProvider, $urlRouterProvider) {
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,14 +1,5 @@
 'use strict';
 
-angular
-  .module('angularDemo')
-  .controller('MainCtrl', MainCtrl)
-  .filter('reverse', function() {
-  return function(text) {
-    return text.split("").reverse("").join("") + text; 
-  }
-});
-
 function MainCtrl ($scope, shared) {
   var main = this;
 
@@ -18,4 +9,41 @@ function MainCtrl ($scope, shared) {
     return msg.split("").reverse("").join("");
   }
 }
+
+function composeEmail () {
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: true,
+    controllerAs: 'compose',
+    controller: function () {
+
+    },
+    link: function ($scope, $element, $attrs) {
+
+    },
+    template: [
+      '<div class="compose-email">',
+        '<input type="text" placeholder="To.." ng-model="compose.to">',
+        '<input type="text" placeholder="Subject.." ng-model="compose.subject">',
+        '<textarea placeholder="Message.." ng-model="compose.message"></textarea>',
+      '</div>'
+    ].join('')
+  };
+}
+
+angular
+  .module('angularDemo')
+
+  //driective: wordt automatisch omgezet naar compose-email
+  .directive('composeEmail', composeEmail)
+
+  .controller('MainCtrl', MainCtrl)
+  .controller('composeEmail', composeEmail)
+  .filter('reverse', function() {
+  return function(text) {
+    return text.split("").reverse("").join("") + text; 
+  }
+});
+
 
